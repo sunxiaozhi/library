138567113964e9150417970dd71e5f8dfb85e93a
sunxiaozhi <1129535445@qq.com>
2018-06-06 21:50:11 +0800

    add captcha

--- src/captcha/Captcha.php
+++ src/captcha/Captcha.php
@@ -1,5 +1,6 @@
 <?php
 /**
+ * 验证码的生成
  * Created by PhpStorm.
  * User: sunxiaozhi
  * Date: 2018/6/6
@@ -10,5 +11,11 @@ namespace sunxiaozhi\library\captcha;
 
 class Captcha
 {
+    /*
+     * 验证码的生成
+     * */
 
+    /*
+     * 验证码的验证
+     * */
 }
